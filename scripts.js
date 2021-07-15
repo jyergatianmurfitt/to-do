d35d0d49ef2c3533abc17747f84e2d6b95100f32
Josephina <jyergatianmurfitt@googlemail.com>
2021-07-15 15:49:57 +0100

    Fixed typo and added completion function

--- scripts.js
+++ scripts.js
@@ -1,22 +1,29 @@
 var app = new function() {
   this.el = document.getElementById('tasks');
   this.tasks = [];
+  this.completedList = [];
+
+
+
 
   this.FetchAll = function() {
     var data = '';
 
     if (this.tasks.length > 0) {
       for (i = 0; i < this.tasks.length; i++) {
-        data += '<tr>';
-        data += '<td>' + (i + 1) + ". " + this.tasks[i] + '</td>';
-        data += '<td><button onclick="app.Edit(' + i + ')" class="btn btn-warning">Edit</button></td>';
-        data += '<td><button onclick="app.Delete(' + i + ')" class="btn btn-danger">Delete</button></td>';
-        data += '</tr>';
+        data += `<tr><td> ${ (i + 1) }. ${ this.tasks[i] } </td>
+        <td><button onclick="app.Edit( ${ i } )" class="btn btn-warning">Edit</button></td>
+        <td><button onclick="app.Delete( ${ i } )" class="btn btn-danger">Delete</button></td>
+        <td><button onclick="app.Completed( ${ i } )" class="btn btn-success">Completed</button></td>
+        </tr>`;
       }
     }
     this.Count(this.tasks.length);
     return this.el.innerHTML = data;
   };
+
+
+
 
   this.Add = function() {
     el = document.getElementById('add-todo');
@@ -28,6 +35,34 @@ var app = new function() {
       this.FetchAll();
     }
   };
+
+
+
+
+  this.CompletedList = function() {
+    var completedData = '';
+
+    if (this.completedList.length > 0) {
+      for (j = 0; j < this.completedList.length; j++) {
+        completedData += `<tr><td class="text-success"> ${ (j + 1) }. ${ this.completedList[j] } </td></tr>`;
+      }
+    }
+    return document.getElementById('completed-list').innerHTML = completedData;
+  }
+
+
+
+  this.Completed = function(item) {
+    var compItem = this.tasks[item];
+
+    this.completedList.push(compItem);
+    this.tasks.splice([item], 1);
+    this.FetchAll();
+    this.CompletedList();
+  }
+
+
+
 
   this.Edit = function(item) {
     el = document.getElementById('edit-todo');
@@ -45,17 +80,23 @@ var app = new function() {
     }
   };
 
+
+
+
   this.Delete = function(item) {
     this.tasks.splice(item, 1);
     this.FetchAll();
   };
 
+
+
+
   this.Count = function(data) {
     var el = document.getElementById('counter');
-    var name = 'Tasks';
+    var name = 'tasks';
     if(data) {
       if(data == 1) {
-        name = 'Task';
+        name = 'task';
       }
       el.innerHTML = data + ' ' + name;
     } else {
